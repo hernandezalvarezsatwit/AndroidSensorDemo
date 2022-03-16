d75e4896b5b17dfcf2964c837f5d542965fa631a
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-16 08:38:16 -0400

    Added onCreate method content

--- app/src/main/java/edu/wit/mobileapp/sensor/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/sensor/MainActivity.java
@@ -2,7 +2,12 @@ package edu.wit.mobileapp.sensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.text);
+        SensorManager mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = mgr.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder message = new StringBuilder(2048);
+        message.append("The sensors on this device are:\n");
+        message.append("===================================\n\n");
+
+        for(Sensor sensor : sensors){
+            message.append(sensor.getName()+"\n");
+            message.append("    Type: "+sensorTypes.get(sensor.getType())+"\n");
+            message.append("    Vendor: "+sensor.getVendor()+"\n");
+            message.append("    Version: "+sensor.getVersion()+"\n");
+            message.append("    Resolutions: "+sensor.getResolution()+"\n");
+            message.append("    Max Range: "+sensor.getMaximumRange()+"\n");
+            message.append("    Power: "+sensor.getPower()+"mA\n\n");
+        }
+
+        text.setText(message);
     }
 }
