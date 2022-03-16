14d0f3546d3232109fb11cf4ba9861a49ee4e069
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-16 08:39:38 -0400

    Added comments

--- app/src/main/java/edu/wit/mobileapp/sensor/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/sensor/MainActivity.java
@@ -17,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView text = findViewById(R.id.text);
+        //Get the SensorManager
         SensorManager mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        //Get List of sensors in device
         List<Sensor> sensors = mgr.getSensorList(Sensor.TYPE_ALL);
 
+        //Message that will hold all information about sensors
         StringBuilder message = new StringBuilder(2048);
         message.append("The sensors on this device are:\n");
         message.append("===================================\n\n");
 
+        //For loop to append information
         for(Sensor sensor : sensors){
             message.append(sensor.getName()+"\n");
             message.append("    Type: "+sensorTypes.get(sensor.getType())+"\n");
